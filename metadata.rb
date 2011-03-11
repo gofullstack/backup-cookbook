a2e567f078c1f34ee485e820901caa49cb764ab2
Nathan L Smith <nlloyds@gmail.com>
2011-03-11 15:03:17 -0600

    backup updates

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,3 @@ license          "All rights reserved"
 description      "Installs/Configures backup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
-depends          "sqlite"
-depends          "xml"
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,38 +6,8 @@
 #
 # All rights reserved - Do Not Redistribute
 
-include_recipe "xml::default" # for nokogiri
-
-execute "backup --setup" do
-  creates "/opt/backup"
-  action :nothing
-end
-
 gem_package "whenever"
 
-# activemodel requires builder (~> 2.1.2, runtime) (Gem::InstallError)
-gem_package "builder" do
-  version "2.1.2"
-end
-
 gem_package "backup" do
-  notifies :run, resources(:execute => "backup --setup")
-end
-
-backups = {}
-
-search(:apps) do |app|
-  (app[:backups] || []).each do |env, app_backups|
-    backups.merge!(app_backups) if env == node[:app_environment]
-  end
-end
-
-p backups
-
-template "/opt/backup/config/backup.rb" do
-  source "backup.rb.erb"
-  mode 0644
-  owner 'root'
-  group 'root'
-  variables :backups => backups
+  version '3.0.2'
 end
