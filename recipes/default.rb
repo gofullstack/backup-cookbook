83e96a037ce4f80a6eadadeee8a072b4dbfcd525
Nathan L Smith <nlloyds@gmail.com>
2011-03-15 15:16:49 -0500

    some backup progress

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,3 +11,37 @@ gem_package "whenever"
 gem_package "backup" do
   version '3.0.6'
 end
+
+# Application-based backups
+search(:apps) do |app|
+  models = []
+  backup_path = "#{app[:deploy_to]}/shared/backup"
+
+  if app.has_key? :backups
+    app[:backups].each_pair do |id, backup|
+      model = backup
+      model[:id] = "#{app[:id]}_#{id}"
+      model[:app_db] = app[:databases][node[:app_environment]]
+      models << model
+    end
+
+    template "#{app[:deploy_to]}/shared/config/backup.rb" do
+      source "app_backup.rb.erb"
+      owner app[:owner] || "root"
+      group app[:group] || "root"
+      mode 0600
+      variables :models => models
+    end
+
+    template "#{app[:deploy_to]}/shared/config/schedule.rb" do
+      source "app_schedule.rb.erb"
+      owner app[:owner] || "root"
+      group app[:group] || "root"
+      mode 0644
+      variables({
+        :models => models,
+        :args => "--log-path=#{backup_path}/log --tmp-path=#{backup_path}/tmp --config-file=#{app[:deploy_to]}/shared/config/backup.rb --data-path=#{backup_path}/data"
+      })
+    end
+  end
+end
