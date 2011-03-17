2786e7100bdcbe4c6eb560f63ed8d38790f58d42
Nathan L Smith <nlloyds@gmail.com>
2011-03-17 11:11:49 -0500

    backups working

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,9 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 
-gem_package "whenever"
+gem_package 'whenever'
 
-gem_package "backup" do
+gem_package 'backup' do
   version '3.0.6'
 end
 
@@ -26,22 +26,27 @@ search(:apps) do |app|
     end
 
     template "#{app[:deploy_to]}/shared/config/backup.rb" do
-      source "app_backup.rb.erb"
-      owner app[:owner] || "root"
-      group app[:group] || "root"
+      source 'app_backup.rb.erb'
+      owner app[:owner] || 'root'
+      group app[:group] || 'root'
       mode 0600
       variables :models => models
     end
 
     template "#{app[:deploy_to]}/shared/config/schedule.rb" do
-      source "app_schedule.rb.erb"
-      owner app[:owner] || "root"
-      group app[:group] || "root"
+      source 'app_schedule.rb.erb'
+      owner app[:owner] || 'root'
+      group app[:group] || 'root'
       mode 0644
       variables({
         :models => models,
         :args => "--log-path=#{backup_path}/log --tmp-path=#{backup_path}/tmp --config-file=#{app[:deploy_to]}/shared/config/backup.rb --data-path=#{backup_path}/data"
       })
+    end
+
+    execute "write crontab for #{app[:id]}" do
+      user = app[:owner] || 'root'
+      command "whenever --write-crontab #{app[:id]} --set path=$PATH --load-file #{app[:deploy_to]}/shared/config/schedule.rb --user #{user}"
     end
   end
 end
