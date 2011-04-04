cd4f5e58d22007b7fbc2114824316ee5b1e71299
Nathan L Smith <nlloyds@gmail.com>
2011-04-04 14:35:59 -0500

    start sys-level backups

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,7 +9,7 @@
 gem_package 'whenever'
 
 gem_package 'backup' do
-  version '3.0.9'
+  version '3.0.14'
 end
 
 # Application-based backups
@@ -17,7 +17,7 @@ search(:apps) do |app|
   models = []
   backup_path = "#{app[:deploy_to]}/shared/backup"
 
-  if app.has_key? :backups
+  if app.has_key? :backups && (node.run_list.roles & app[:server_roles]).length > 0
     app[:backups].each_pair do |id, backup|
       model = backup
       model[:id] = "#{app[:id]}_#{id}"
@@ -47,6 +47,48 @@ search(:apps) do |app|
     execute "write crontab for #{app[:id]}" do
       user = app[:owner] || 'root'
       command "whenever --write-crontab #{app[:id]} --set path=$PATH --load-file #{app[:deploy_to]}/shared/config/schedule.rb --user #{user}"
+    end
+  end
+end
+
+# Backups from "backups" data bag
+backup_path = (node[:backup] || {})[:path] || '/var/backups'
+user = (node[:backup] || {})[:user] || 'root'
+
+search(:backups) do |bag|
+  if (bag[:nodes] || []).include?(node.name)
+    models = []
+
+    if bag.has_key? :backups
+      bag[:backups].each_pair do |id, backup|
+        model = backup
+        model[:id] = "#{bag[:id]}_#{id}"
+        model[:app_db] = {} # not used
+        models << model
+      end
+
+      template "#{backup_path}/backup.rb" do
+        source 'app_backup.rb.erb'
+        owner user
+        group user
+        mode 0600
+        variables :models => models
+      end
+
+      template "#{backup_path}/schedule.rb" do
+        source 'app_schedule.rb.erb'
+        owner user
+        group user
+        mode 0644
+        variables({
+          :models => models,
+          :args => "--log-path=#{backup_path}/log --tmp-path=#{backup_path}/tmp --config-file=#{backup_path}/backup.rb --data-path=#{backup_path}/data"
+        })
+      end
+
+      execute "write crontab for #{bag[:id]}" do
+        command "whenever --write-crontab #{bag[:id]} --set path=$PATH --load-file #{backup_path}/schedule.rb --user #{user}"
+      end
     end
   end
 end
