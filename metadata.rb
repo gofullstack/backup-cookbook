1963b70587f6237adfc2be762d90215e83769cdf
Nathan L Smith <nlloyds@gmail.com>
2012-10-26 23:05:05 -0500

    move readme to markdown; bump version

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ maintainer       "Cramer Development, Inc."
 maintainer_email "sysadmin@cramerdev.com"
 license          "Apache 2.0"
 description      "Installs/Configures backup"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
 depends          "xml"
