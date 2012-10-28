7710f1621065edb671881a0ec6d705ddec11df52
Nathan L Smith <nlloyds@gmail.com>
2012-10-27 19:15:30 -0500

    remove apps data bag based backup capability

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: backup
 # Recipe:: default
 #
-# Copyright 2011, Cramer Development, Inc.
+# Copyright 2011-2012, Cramer Development, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,45 +31,6 @@ if node['languages']['ruby']['version'][0..2] == '1.8'
   gem_package 's3sync'
 else
   gem_package 'aproxacs-s3sync'
-end
-
-# Application-based backups
-search(:apps) do |app|
-  models = []
-  backup_path = "#{app[:deploy_to]}/shared/backup"
-
-  if app.has_key? :backups && (node.run_list.roles & app[:server_roles]).length > 0
-    app[:backups].each_pair do |id, backup|
-      model = backup
-      model[:id] = "#{app[:id]}_#{id}"
-      model[:app_db] = app[:databases][node[:app_environment]]
-      models << model
-    end
-
-    template "#{app[:deploy_to]}/shared/config/backup.rb" do
-      source 'app_backup.rb.erb'
-      owner app[:owner] || 'root'
-      group app[:group] || 'root'
-      mode 0600
-      variables :models => models
-    end
-
-    template "#{app[:deploy_to]}/shared/config/schedule.rb" do
-      source 'app_schedule.rb.erb'
-      owner app[:owner] || 'root'
-      group app[:group] || 'root'
-      mode 0644
-      variables({
-        :models => models,
-        :args => "--log-path=#{backup_path}/log --tmp-path=#{backup_path}/tmp --config-file=#{app[:deploy_to]}/shared/config/backup.rb --data-path=#{backup_path}/data"
-      })
-    end
-
-    execute "write crontab for #{app[:id]}" do
-      user = app[:owner] || 'root'
-      command "whenever --write-crontab #{app[:id]} --set path=$PATH --load-file #{app[:deploy_to]}/shared/config/schedule.rb --user #{user}"
-    end
-  end
 end
 
 # Backups from "backups" data bag
