118c0c35a0d9480e712f602d078cdd4e46c42d1a
Nathan L Smith <nlloyds@gmail.com>
2012-10-31 12:18:58 -0500

    remove app based backups

--- recipes/default.rb
+++ recipes/default.rb
@@ -33,45 +33,6 @@ else
   gem_package 'aproxacs-s3sync'
 end
 
-# Application-based backups
-search(:apps) do |app|
-  models = []
-  backup_path = "#{app[:deploy_to]}/shared/backup"
-
-  if app.has_key? :backups && (node.run_list.roles & app[:server_roles]).length > 0
-    app[:backups].each_pair do |id, backup|
-      model = backup
-      model[:id] = "#{app[:id]}_#{id}"
-      model[:app_db] = app[:databases][node[:app_environment]]
-      models << model
-    end
-
-    template "#{app[:deploy_to]}/shared/config/backup.rb" do
-      source 'app_backup.rb.erb'
-      owner app[:owner] || 'root'
-      group app[:group] || 'root'
-      mode 0600
-      variables :models => models
-    end
-
-    template "#{app[:deploy_to]}/shared/config/schedule.rb" do
-      source 'app_schedule.rb.erb'
-      owner app[:owner] || 'root'
-      group app[:group] || 'root'
-      mode 0644
-      variables({
-        :models => models,
-        :args => "--log-path=#{backup_path}/log --tmp-path=#{backup_path}/tmp --config-file=#{app[:deploy_to]}/shared/config/backup.rb --data-path=#{backup_path}/data"
-      })
-    end
-
-    execute "write crontab for #{app[:id]}" do
-      user = app[:owner] || 'root'
-      command "whenever --write-crontab #{app[:id]} --set path=$PATH --load-file #{app[:deploy_to]}/shared/config/schedule.rb --user #{user}"
-    end
-  end
-end
-
 # Backups from "backups" data bag
 backup_path = node['backup']['path'] || '/var/backups'
 user = node['backup']['user'] || 'root'
