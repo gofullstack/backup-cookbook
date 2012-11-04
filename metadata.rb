862b41e81b474562cf3aed3397f59c05136c3bd8
Nathan L Smith <nlloyds@gmail.com>
2012-11-03 23:02:06 -0500

    remove legacy code

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,3 @@ license          "Apache 2.0"
 description      "Installs/Configures backup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-depends          "xml"
