a120cac20f70d5a790dd2cbca1c3e6dc9bcee36e
Guilhem Lettron <guilhem@lettron.fr>
2013-07-01 12:46:03 +0200

    Revert "use global variable for new_resource"
    This reverts commit 47b5c8351ed7ef0b42d1e94d9a867d9c6a6045f1.
    
    Conflicts:
    	providers/model.rb

--- providers/model.rb
+++ providers/model.rb
@@ -4,25 +4,25 @@ def whyrun_supported?
 end
 
 action :create do
-  cron_options = @new_resource.cron_options || {}
+  cron_options = new_resource.cron_options || {}
 
   cron cron_name do
     command cron_options[:command] ||
-      "backup perform --trigger #{@new_resource.name} --config-file #{node['backup']['config_path']}/config.rb --log-path=#{node['backup']['log_path']} > /dev/null"
+      "backup perform --trigger #{new_resource.name} --config-file #{node['backup']['config_path']}/config.rb --log-path=#{node['backup']['log_path']} > /dev/null"
 
     mailto cron_options[:mailto] if cron_options.key?(:mailto)
     path cron_options[:path] if cron_options.key?(:path)
     shell cron_options[:shell] if cron_options.key?(:shell)
     user cron_options[:user] || node['backup']['user']
 
-    minute @new_resource.schedule[:minute] || '*'
-    hour @new_resource.schedule[:hour] || '*'
-    day @new_resource.schedule[:day] || '*'
-    month @new_resource.schedule[:month] || '*'
-    weekday @new_resource.schedule[:weekday] || '*'
+    minute new_resource.schedule[:minute] || '*'
+    hour new_resource.schedule[:hour] || '*'
+    day new_resource.schedule[:day] || '*'
+    month new_resource.schedule[:month] || '*'
+    weekday new_resource.schedule[:weekday] || '*'
   end
 
-  template "Model file for #{@new_resource.name}" do
+  template "Model file for #{new_resource.name}" do
     path model_path
     source model.erb
     owner node['backup']['user']
@@ -42,7 +42,7 @@ action :delete do
     action :delete
   end
 
-  file "Model file for #{@new_resource.name}" do
+  file "Model file for #{new_resource.name}" do
     path model_path
     action :delete
   end
@@ -51,9 +51,9 @@ end
 private
 
 def cron_name
-  "#{@new_resource.name}_backup"
+  "#{new_resource.name}_backup"
 end
 
 def model_path
-  ::File.join(node['backup']['model_path'], "#{@new_resource.name}.rb")
+  ::File.join(node['backup']['model_path'], "#{new_resource.name}.rb")
 end
