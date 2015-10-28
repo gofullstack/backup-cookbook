2f20a1467888e4aff547303100f0bd831f2cb1d3
Philip Douglas <philip_douglas@sn.scee.net>
2015-10-28 10:49:04 +0000

    Fix crash under chef-client 12.5
    chef-client 12.5 doesn't allow name attributes to be Symbols, so change
    to be a String

--- resources/model.rb
+++ resources/model.rb
@@ -1,7 +1,7 @@
 actions :create, :delete
 default_action :create
 
-attribute :name, :kind_of => Symbol, :name_attribute => true, :required => true
+attribute :name, :kind_of => String, :name_attribute => true, :required => true
 attribute :description, :kind_of => String
 
 attribute :definition, :kind_of => String
