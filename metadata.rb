a96e516d899916a73753f31129b3f005e37c545a
Ali Yahya <amyahya@gmail.com>
2017-05-31 14:07:51 -0400

    Use file for version.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ maintainer_email "sysadmin@cramerdev.com"
 license          "Apache 2.0"
 description      "Installs/Configures backup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.4.0"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '1.4.0'
+
 depends          "cron"
 depends          "gem_specific_install"
